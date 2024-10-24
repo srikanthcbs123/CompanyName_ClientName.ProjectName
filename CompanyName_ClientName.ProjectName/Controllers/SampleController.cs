e04a90820cf7347ba8f8c6daaf499715df9d623d
Dell <Dell@DESKTOP-AAO14OC>
2024-10-24 07:03:30 +0530

    Add project files.

---
+++ CompanyName_ClientName.ProjectName/Controllers/SampleController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CompanyName_ClientName.ProjectName.Controllers
+{
+    [Route("api/[controller]")]//parent route declare here
+    [ApiController]
+    public class SampleController : ControllerBase
+    {
+        [HttpGet]//get api method
+        [Route("Getstringdata")]//child routes define here
+        public string listofdata()
+        {
+            var result = "It Department";
+            return result;
+        }
+        [HttpPost]//post api method
+        [Route("insertdata")]//child routes define here
+        public int insertdata(int a, int b)
+        {
+            return a + b;
+        }
+        [HttpPut]
+        [Route("update data")]//child routes define here
+        public int updatedata(int a, int b)
+        {
+            return a - b;
+        }
+        [HttpDelete]
+        [Route("deletedata")]//child routes define here
+        public int deletedata(int a, int b)
+        {
+            return a - b;
+
+        }
+    }
+}
